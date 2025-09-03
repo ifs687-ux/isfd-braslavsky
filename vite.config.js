9855ecc6c80b782ddff17096a681a35e363c891e
ifs687-ux <ifs687@gmail.com>
2025-09-03 03:03:41 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/isfd-braslavsky/',   // 👈 nombre EXACTO del repo
+  base: '/isfd-braslavsky/',   // nombre EXACTO del repo
 })
